ed707ea3c3f307ee21100b1136cb69cd6c1a9881
EthanCoch <ethan.cochard@isen-ouest.yncrea.fr>
2025-06-14 14:56:28 +0200

    calcule_point

--- game.c
+++ game.c
@@ -19,3 +19,33 @@ Bool discipline_choice()
 // La table de combat
 // Boucle jusqu’à ce que l’un des deux meure
 // Dans un fichier (RC Tir Degats Ennemi Degats Heros)
+
+
+void calcule_point(int rc, int nbr_rand, int* hero,int* enemi){
+    FILE * file = fopen("tab.txt", "r");
+    
+    if (file == NULL) {
+        printf("Unable to open file <tab.txt>\n"), exit(EXIT_FAILURE);
+    }
+
+    char line[50];
+    int rc_tab, nbr_rand_tab, hero_tmp, enemi_tmp;
+
+    while (fgets(line, sizeof(line), file)) {
+        sscanf(line, "%d %d %d %d", &rc_tab, &nbr_rand_tab, hero, enemi);
+
+        if (sscanf(line, "%d %d %d %d", &rc_tab, &nbr_rand_tab, &enemi_tmp, &hero_tmp) == 4) {
+            if (rc == rc_tab && nbr_rand == nbr_rand_tab) {
+                *hero = hero_tmp;
+                *enemi = enemi_tmp;
+                fclose(file);
+                return;
+            }
+        }
+
+    }
+
+    *hero=0;
+    *enemi=0;
+    fclose(file);
+}
