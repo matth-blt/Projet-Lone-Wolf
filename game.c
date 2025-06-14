7d2f0f517959c72f60dc6cc647bd3deecbb710f4
EthanCoch <ethan.cochard@isen-ouest.yncrea.fr>
2025-06-14 10:07:18 +0200

    Update game.c

--- game.c
+++ game.c
@@ -10,3 +10,8 @@ Player * player_generator(char name[256])
     p1->endurance = generate_rnt() + 20;
     return p1;
 }
+//RC = ton HC – HC de l’ennemi (limites le RC entre –11 et +11 s’il dépasse)
+//chiffre aléatoire de 0 à 9
+// la table de combat
+// Boucle jusqu’à ce que l’un des deux meure
+// dans un fichier (RC Tir DegatsEnnemi DegatsHeros)
