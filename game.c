f467b092d6ef57bfe16fc5f9541f289caa161c29
Lia-0106 <lia.arwenn106@gmail.com>
2025-06-14 18:14:21 +0200

    Update game.c

--- game.c
+++ game.c
@@ -165,8 +165,6 @@ void calcule_point(int rc, int nbr_rand, int * hero, int * enemi)
     int rc_tab, nbr_rand_tab, hero_tmp, enemi_tmp;
 
     while (fgets(line, sizeof(line), file)) {
-        sscanf(line, "%d %d %d %d", &rc_tab, &nbr_rand_tab, hero, enemi);
-
         if (sscanf(line, "%d %d %d %d", &rc_tab, &nbr_rand_tab, &enemi_tmp, &hero_tmp) == 4) {
             if (rc == rc_tab && nbr_rand == nbr_rand_tab) {
                 *hero = hero_tmp;
